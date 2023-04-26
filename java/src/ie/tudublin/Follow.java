efd2a6ee9c3825dead2c5bf516ba9693b7eac2c8
ZoeFA <d22125460@mytudublin.ie>
2023-04-26 16:13:34 +0100

    set up fields

---
+++ java/src/ie/tudublin/Follow.java
@@ -0,0 +1,24 @@
+package ie.tudublin;
+
+//start of class
+public class Follow {
+
+  String word;
+  int count;
+
+  //getters and setters
+  public String getWord() {
+    return word;
+  }
+  public void setWord(String word) {
+    this.word = word;
+  }
+  public int getCount() {
+    return count;
+  }
+  public void setCount(int count) {
+    this.count = count;
+  }
+
+  
+}
